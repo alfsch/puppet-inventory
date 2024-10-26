bc777b29fcdabc6b83d178aa600a90a24241018d
Alfred Schmid <A.Schmid@ff-muenchen.de>
2024-10-26 21:05:00 +0200

    invalidate puppet cache after r10k is executed

--- local-testing/scripts/update-inventory.sh
+++ local-testing/scripts/update-inventory.sh
@@ -2,3 +2,8 @@
 
 /usr/local/bin/r10k deploy environment -m
 
+curl -i --cert $(puppet config print hostcert) \
+  --key $(puppet config print hostprivkey) \
+  --cacert $(puppet config print cacert) \
+  -X DELETE \
+  https://$(puppet config print server):8140/puppet-admin-api/v1/environment-cache
